0c08ad6bf89020f95eb29ca3acfb5464043abe3a
SemenutaEgor <e.semenuta@yandex.ru>
2020-07-13 17:08:58 +0300

    added binding a socket

--- PNet/Server/Source.cpp
+++ PNet/Server/Source.cpp
@@ -10,7 +10,9 @@ int main()
 	{
 		std::cout << "Winsock api successfully initialized." << std::endl;
 
-		IPEndpoint test("www.google.com", 8080); 
+		//server to listen for connections on port 4790
+
+		/*IPEndpoint test("www.google.com", 8080); 
 		if (test.GetIPVersion() == IPVersion::IPv4)
 		{
 			std::cout << "Hostmane: " << test.GetHostName() << std::endl;
@@ -25,12 +27,20 @@ int main()
 		else
 		{
 			std::cerr << "This is not an ipv4 address." << std::endl;
-		}
+		}*/
 
 		Socket socket;
 		if (socket.Create() == PResult::P_Success)
 		{
 			std::cout << "Socket successfully created." << std::endl;
+			if (socket.Bind(IPEndpoint("0.0.0.0", 4790)) == PResult::P_Success) 
+			{
+				std::cout << "Socket successfully bound to  port 4790." << std::endl;
+			}
+			else
+			{
+				std::cerr << "Failed to bind socket  to port 4790." << std::endl;
+			}
 			socket.Close();
 		}
 		else
