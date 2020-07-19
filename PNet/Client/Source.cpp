a2784493f9ce0398375745035947bee56c801a58
SemenutaEgor <e.semenuta@yandex.ru>
2020-07-19 04:12:31 +0300

    hide passwords

--- PNet/Client/Source.cpp
+++ PNet/Client/Source.cpp
@@ -2,8 +2,25 @@
 #include "MyClient.h"
 #include <iostream>
 #include <thread>
+#include <conio.h>
 
 MyClient client;
+
+std::string HidePassword()
+{
+	std::vector<char> vectorPassword;
+	char c;
+
+	std::cout << "Enter password: " << std::endl;
+	while ((c = _getch()) != '\r')
+	{
+		vectorPassword.push_back(c);
+		_putch('*');
+	}
+
+	std::string stringPassword(vectorPassword.begin(), vectorPassword.end());
+	return stringPassword;
+}
 
 void loading()
 {
@@ -25,7 +42,7 @@ int main()
 			std::string userpassword = "";
 			while (com != 0)
 			{
-				std::cout << "Sign in - 1, Sign up - 2, MixWord - 3, Exit - 0" << std::endl;
+				std::cout << "\nSign in - 1, Sign up - 2, MixWord - 3, Exit - 0" << std::endl;
 				std::cin >> com;
 				switch (com)
 				{
@@ -34,9 +51,10 @@ int main()
 					std::cout << "Enter username:" << std::endl;
 					//std::string username;
 					std::cin >> username;
-					std::cout << "Enter password:" << std::endl;
+					//std::cout << "Enter password:" << std::endl;
 					//std::string userpassword;
-					std::cin >> userpassword;
+					//std::cin >> userpassword;
+					userpassword = HidePassword();
 					client.SendAccountCredentials(username, userpassword);
 					//client.Frame();
 					break;
@@ -46,9 +64,10 @@ int main()
 					std::cout << "Enter username:" << std::endl;
 					//std::string username;
 					std::cin >> username;
-					std::cout << "Enter password:" << std::endl;
+					//std::cout << "Enter password:" << std::endl;
 					//std::string userpassword;
-					std::cin >> userpassword;
+					//std::cin >> userpassword;
+					userpassword = HidePassword();
 					client.SendNewAccount(username, userpassword);
 					//client.Frame();
 					break;
